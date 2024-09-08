a35c8332edb7cfcbf47cb5ac0f54f231c419eb82
XiyanFlowC <1197129380@qq.com>
2024-09-08 14:39:28 +0800

    脚本文件处理

--- FFXIVClassicDat/LuaScriptFile.cpp
+++ FFXIVClassicDat/LuaScriptFile.cpp
@@ -1,9 +1,48 @@
 #include "LuaScriptFile.h"
 
+#include <filesystem>
+#include <fstream>
+#include <cstring>
 #include "xybase/xystring.h"
 
 std::wstring LuaScriptFile::subalphabet = L"0123456789abcdefghijklmnopqrstuvwxyz";
 std::wstring LuaScriptFile::revalphabet = L"jihgfedcba9876543210zyxwvutsrqponmlk";
+
+LuaScriptFile::LuaScriptFile(const std::wstring &p_scriptBasePath)
+{
+	basePath = p_scriptBasePath;
+	std::wstring p_staticActorSanPath = p_scriptBasePath + FileNameCipher(L"StaticActor") + L".san";
+	size_t length = std::filesystem::file_size(p_staticActorSanPath);
+	std::ifstream eye(p_staticActorSanPath, std::ios::binary);
+	SanHeader header;
+	eye.read((char *) &header, sizeof(SanHeader));
+	if (memcmp(&header.magic, "sane", 4))
+	{
+		throw xybase::InvalidParameterException(L"p_staticActorSanPath", L"Magic Header Verification failed.", 187200);
+	}
+	size_t recordLength = length - sizeof(SanHeader);
+	char *buffer = new char[recordLength];
+	eye.read(buffer, recordLength);
+
+	for (int i = 0; i < recordLength; ++i)
+	{
+		buffer[i] ^= 0x73;
+	}
+
+	char *cur = buffer;
+	while (cur <= buffer + length)
+	{
+		Actor *actor = (Actor *)cur;
+		std::u8string name{ (char8_t *)actor->name };
+		actors[actor->id] = name;
+		size_t alignedLength = (name.size() + 1) + 3 & ~3;
+		cur += 4 + alignedLength;
+	}
+
+
+	delete[] buffer;
+	eye.close();
+}
 
 std::wstring LuaScriptFile::FileNameCipher(std::wstring_view p_fileName)
 {
@@ -19,10 +58,7 @@ std::wstring LuaScriptFile::FileNameCipher(std::wstring_view p_fileName)
 		else
 			ret += ch;
 	}
-}
-
-LuaScriptFile::LuaScriptFile(const std::wstring &p_staticActorSanPath)
-{
+	return ret;
 }
 
 std::wstring LuaScriptFile::FileNameDecipher(std::wstring_view p_fileName)
@@ -38,4 +74,47 @@ std::wstring LuaScriptFile::FileNameDecipher(std::wstring_view p_fileName)
 		else
 			ret += ch;
 	}
+	return ret;
+}
+
+BinaryData LuaScriptFile::GetLuacDataByPath(const std::wstring &path)
+{
+	if (!std::filesystem::exists(path))
+		throw xybase::InvalidParameterException(L"path", L"Specifed actor not found.", 107210);
+
+	size_t length = std::filesystem::file_size(path);
+	char *contents = new char[length];
+	std::ifstream eye(path, std::ios::binary);
+	eye.read(contents, length);
+	eye.close();
+
+	LpdHeader *hdr = (LpdHeader *)contents;
+
+	if (memcmp(hdr->magic, "rle\x0c", 4))
+	{
+		throw xybase::InvalidParameterException(L"path", L"Not a valid lpd.", 107211);
+	}
+
+	BinaryData ret(&hdr->script, hdr->fileSize);
+
+	char *ptr = (char *)ret.GetData();
+	for (int i = 0; i < ret.GetLength(); ++i)
+	{
+		*ptr++ ^= 0x73;
+	}
+
+	delete[] contents;
+	return ret;
+}
+
+BinaryData LuaScriptFile::GetLuacDataByName(const std::u8string &actorName)
+{
+	std::wstring lpdPath = basePath + L"\\" + FileNameCipher(xybase::string::to_wstring(actorName)) + L"_p.le.lpd";
+	return GetLuacDataByPath(lpdPath);
+}
+
+
+BinaryData LuaScriptFile::GetLuacDataByActorId(uint32_t id)
+{
+	GetLuacDataByName(actors.find(id)->second);
 }
--- FFXIVClassicDat/LuaScriptFile.h
+++ FFXIVClassicDat/LuaScriptFile.h
@@ -2,20 +2,60 @@
 
 #include <string>
 #include "BinaryData.h"
+#include <map>
 
 class LuaScriptFile
 {
 	static std::wstring subalphabet;
 	static std::wstring revalphabet;
-public:
+protected:
+	std::map<uint32_t, std::u8string> actors;
+	std::wstring basePath;
 
-	LuaScriptFile(const std::wstring &p_staticActorSanPath);
+public:
+	struct SanHeader
+	{
+		char magic[4];
+		char ukn[9];
+	};
+
+	struct Actor
+	{
+		uint32_t id;
+		char name[1]; // 标志用，长度变化
+	};
+
+	struct LpdHeader
+	{
+		char magic[4];
+		uint32_t version;
+		uint32_t fileSize;
+		char ukn;
+		char script[1];
+	};
 
 	/**
-	 * @brief 根据给出的路径，查询staticactor，加密后获取实际文件。进行解密后，返回明文的裸Luac代码。
+	 * @brief 初始化脚本访问工具。需给出脚本基本路径。路径下需要描述脚本对应关系的
+	 * staticactor.san存在才可正常运作。
+	 */
+	LuaScriptFile(const std::wstring &p_scriptBasePath);
+
+
+	BinaryData GetLuacDataByPath(const std::wstring &path);
+
+	/**
+	 * @brief 根据给出的Actor名，查询staticactor，加密路径后获取实际文件。进行解密后，返回明文的裸Luac代码。
 	 * 可使用Unluac等进一步处理
 	 */
-	BinaryData GetLuacDataByName(const std::wstring &path);
+	BinaryData GetLuacDataByName(const std::u8string &actorName);
+
+	/**
+	 * @brief 使用ActorId获取文件
+	 * @note 该ActorId和游戏内部Id对应关系未确认
+	 * @param id 
+	 * @return 
+	 */
+	BinaryData GetLuacDataByActorId(uint32_t id);
 
 	/**
 	 * @brief 解密脚本文件名。（简单的代替密码
